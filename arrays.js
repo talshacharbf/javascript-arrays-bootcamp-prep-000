e309f513b5f64c35f8b4a11d9ecee084ae17f89c
tal shachar <tal.shachar@buzzfeed.com>
2017-11-13 20:01:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array, element) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array.slice(1)
+  array.slice(-1)
   return array
 }
