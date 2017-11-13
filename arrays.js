01482add21bc82b198bb142b486a9af73345a394
tal shachar <tal.shachar@buzzfeed.com>
2017-11-13 20:06:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,7 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   array.pop(1)
   return array
 }
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, array.length-1)
+}
