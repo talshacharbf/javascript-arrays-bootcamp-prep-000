28010527ed8054e720177eb1e364b17ab39a308a
tal shachar <tal.shachar@buzzfeed.com>
2017-11-13 19:22:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  arrary.unshift.(element)
+  array.unshift.(element)
 }
